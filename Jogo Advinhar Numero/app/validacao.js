234f68b0e599befc1f766bc0186c46324cad2428
AugustoSTavares <127952712+AugustoSTavares@users.noreply.github.com>
2023-04-24 14:04:23 -0300

    Add files via upload

---
+++ Jogo Advinhar Numero/app/validacao.js
@@ -0,0 +1,44 @@
+function verificaValidadeDoNumero(chute){
+    const numero =+chute 
+
+    if  (valorInvalido(numero)){
+        elementoChute.innerHTML += `<div> Valor inválido: Fale um número entre ${menorValor} e ${maiorValor} <div>`
+        return
+    }
+
+    if (valorMaiorOuMenorQueOPermitido(numero)){
+        elementoChute.innerHTML += `<div> Valor inválido: Fale um número entre ${menorValor} e ${maiorValor} <div>`
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML=`
+        <h2> Você acertou!<h2>
+        <h3> O número secreto era ${numeroSecreto} <h3>
+
+        <button id="jogar-novamente" class="botao__jogar"> Jogar Novamente </button>
+        `
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += 
+        `<div>O número secreto é menor <i class="fa-sharp fa-solid fa-angle-down"></i> </div>`
+    }
+
+    else {
+        elementoChute.innerHTML += 
+        `<div>O número secreto é maior <i class="fa-sharp fa-solid fa-angle-up"></i> </div>`
+    }
+}
+
+function valorInvalido(numero){
+   return Number.isNaN(numero)
+}
+
+function valorMaiorOuMenorQueOPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e=>{
+    if (e.target.id =='jogar-novamente'){
+        window,location.reload()
+    }
+})
